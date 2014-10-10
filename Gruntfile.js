2ed2d72879eada90439e72d468fc6b62a36e8a9e
Matt Gauger <matt.gauger@gmail.com>
2014-10-10 16:09:18 -0500

    Files to copy to gh-pages deploy dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,27 @@ module.exports = function(grunt) {
       sass: {
         files: 'sass/**/*.scss',
         tasks: ['sass'],
+        options: {}
+      },
+      copyto: {
+        files: 'out/**',
+        tasks: ['copyto'],
+        options: {}
+      }
+    },
+    copyto: {
+      ghpages: {
+        files: [
+          {cwd: 'out/', src: ['**/*'], dest: 'gh-pages/', expand: true}
+        ],
         options: {
+          // array of ignored paths, can be specific files or a glob
+          ignore: [
+            'stuffdir/**/*.bak',
+            'stuffdir/dontcopyme.txt',
+            // ignore both a directory and it's contents (brace expansion)
+            'stuffdir/somedir{,/**/*}'
+          ]
         }
       }
     }
@@ -23,6 +43,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', ['sass']);
 
-  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-copy-to');
+  grunt.loadNpmTasks('grunt-sass');
 };
