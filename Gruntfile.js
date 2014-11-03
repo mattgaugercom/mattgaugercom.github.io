bfcf16e63b327610e123d1053d0c7b7b91280459
Matt Gauger <matt.gauger@gmail.com>
2014-11-03 07:48:05 -0600

    Begin building deploy task
    - Not fully automated yet.

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,6 +42,7 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('default', ['sass']);
+  grunt.registerTask('deploy', ['copyto']);
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-copy-to');
