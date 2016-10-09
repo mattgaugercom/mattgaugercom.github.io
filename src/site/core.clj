58ab2bc4e00ab95b2c43a8d695726a8f0530d4e9
Matt Gauger <matt.gauger@gmail.com>
2016-10-08 19:46:23 -0500

    Get the CSS URL right

--- src/site/core.clj
+++ src/site/core.clj
@@ -43,7 +43,7 @@
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
     [:link {:rel "shortcut icon" :type "image/x-icon" :href "favicon.ico"}]
     [:title "mattgauger.com"]
-    (hp/include-css "https://unpkg.com/reeeset@0.0.7.css")
+    (hp/include-css "https://unpkg.com/reeeset@0.0.7")
     (hp/include-css "/css/index.css")
     (hp/include-js "https://unpkg.com/headroom.js")]
    [:body
